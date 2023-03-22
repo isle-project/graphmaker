3fd05a760b9eb7a9aae75a622752d604fed428a0
Christopher R. Genovese <genovese@cmu.edu>
2023-03-22 13:47:35 -0400

    fix: added test file and svd-js in package.json again

---
+++ test/position-test.js
@@ -0,0 +1,96 @@
+const { constrainedEquilibrate
+      , nodePositions
+      , UNCONSTRAINED
+      } = require( '../lib/node-position.js' );
+
+
+// Constants
+
+const TOLERANCE = 2 ** -18;
+const EQ_OPTIONS = { tol: TOLERANCE };
+const DIGITS_TO_ZERO = 6;
+
+
+// Helpers
+
+const euclidean = (x, y) => {
+    const d0 = x[0] - y[0];
+    const d1 = x[1] - y[1];
+    return Math.sqrt( d0 * d0 + d1 * d1 );
+};
+
+const norm = (x) => {
+    const d0 = x[0];
+    const d1 = x[1];
+    return Math.sqrt( d0 * d0 + d1 * d1 );
+};
+
+const mmul = (M, v) => {
+    if ( M[0].length === v.length ) {
+        return M.map( row => dot( row, v ) );
+    }
+    return (void 0);
+};
+
+/** Scales a vector (array) by a constant. */
+const vscale = (v, c) => v.map( y => c * y );
+
+/** Computes affine transformation a x + b y + c 1 for vectors (arrays) x, y, 1 */
+const vaffine = (x, y, a = 1, b = 1, c = 0) => x.map( (val, ind) => a*val + b*y[ind] + c );
+
+
+
+// Tests
+
+test( 'simple-position-symmetry',  () => {
+    let actual;
+    let diff;
+
+    // Single point at the origin
+    actual = constrainedEquilibrate( [ [1,2] ], UNCONSTRAINED, UNCONSTRAINED, EQ_OPTIONS );
+    expect( actual.converged ).toBeTruthy();
+    expect( norm(actual.positions[0]) ).toBeCloseTo( 0, DIGITS_TO_ZERO );
+
+    actual = nodePositions( 1, UNCONSTRAINED, UNCONSTRAINED, EQ_OPTIONS );
+    expect( actual.converged ).toBeTruthy();
+    expect( norm(actual.positions[0]) ).toBeCloseTo( 0, DIGITS_TO_ZERO );
+
+    // Two points reflections of each other relative to the origin
+    actual = nodePositions( 2, UNCONSTRAINED, UNCONSTRAINED, EQ_OPTIONS );
+    expect( actual.converged ).toBeTruthy();
+    diff = [ actual.positions[0][0] + actual.positions[1][0]
+           , actual.positions[0][1] + actual.positions[1][1]
+           ];
+    expect( norm(diff) ).toBeCloseTo( 0, DIGITS_TO_ZERO );
+
+
+    // Three points form an equliteral triangle around the origin
+    actual = nodePositions( 3, UNCONSTRAINED, UNCONSTRAINED, EQ_OPTIONS );
+    expect( actual.converged ).toBeTruthy();
+    expect( actual.positions.reduce( (avg, x) => avg + x/3, 0) ).toBeCloseTo( 0,  DIGITS_TO_ZERO );
+    // ATTN: More specific test?
+
+    // Four points form a square around the origin
+    actual = nodePositions( 4, UNCONSTRAINED, UNCONSTRAINED, EQ_OPTIONS );
+    expect( actual.converged ).toBeTruthy();
+    expect( actual.positions.reduce( (avg, x) => avg + x/4, 0) ).toBeCloseTo( 0,  DIGITS_TO_ZERO );
+    // ATTN: More specific test?
+});
+
+test( 'simple-equality-constraints', () => {
+    let actual;
+    let init;
+
+    const midpointConstraint = { matrix: [ [0.5, 0, 0.5, 0, -1, 0], [0, 0.5, 0, 0.5, 0, -1] ], rhs: [ 0, 0] };
+    init = [ [1,2], [-1,-2], [-1,2] ];
+    actual = constrainedEquilibrate( init, midpointConstraint, UNCONSTRAINED, EQ_OPTIONS );
+    expect( actual.converged ).toBeTruthy();
+    expect( norm(mmul(midpointConstraint.matrix, actual.positions.flat())) ).toBeCloseTo( 0, DIGITS_TO_ZERO );
+    
+
+    const samecol3Constraint = { matrix: [ [1, 0, -1, 0, 0, 0, 0, 0, 0, 0], [1, 0, 0, 0, -1, 0, 0, 0, 0, 0] ], rhs: [ 0, 0] };
+    init = [ [1,2], [-1,-2], [-1,2], [-0.5, 1], [1,-3] ];
+    actual = constrainedEquilibrate( init, midpointConstraint, UNCONSTRAINED, EQ_OPTIONS );
+    expect( actual.converged ).toBeTruthy();
+    expect( norm(mmul(samecol3Constraint.matrix, actual.positions.flat())) ).toBeCloseTo( 0, DIGITS_TO_ZERO );
+});
