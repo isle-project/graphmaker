d85f0ff8ce88022a3b61a168c2ce6a3faaeeab75
Philipp Burckhardt <pburckhardt@outlook.com>
2023-05-22 15:16:14 -0400

    Update logging and bug fixes
    Co-authored-by: Christopher Genovese <genovese@cmu.edu>

--- bin/gm.sh
+++ bin/gm.sh
@@ -4,4 +4,4 @@ echo '' >> ./run.log
 echo '-------------------' >> ./run.log
 date >> ./run.log
 
-DEBUG_DEPTH=10000 DEBUG=graphmaker* node lib/repl.js 2>> ./run.log
+DEBUG_MAX_STRING_LENGTH=50000 DEBUG_DEPTH=10000 DEBUG=graphmaker* node lib/repl.js 2>> ./run.log
