434638da1dcf3ac824bc8a3a1fb81fffc63158f4
Philipp Burckhardt <pburckhardt@outlook.com>
2023-06-28 15:48:27 -0400

    update paths

--- scripts/build_gallery.js
+++ scripts/build_gallery.js
@@ -15,7 +15,7 @@ function generateGalleryItemPage({ name, svg, json, tasks }) {
       .replace(/height="([^"]*)"/, '');
 
     let body = dedent`
-      <div class="container mx-auto px-4 py-2 mt-2">
+      <div class="container mx-auto px-4 py-2 pt-16 mt-2">
         <div class="flex flex-wrap">
           <div class="w-full md:w-1/2 mb-8 md:mb-0 pl-2 pr-2 relative">
             <div class="bg-white rounded-lg overflow-hidden shadow-md p-8">
