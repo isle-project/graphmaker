e89483b25ec44617117cfa93ad12ce1e51a5b978
Philipp Burckhardt <pburckhardt@outlook.com>
2023-06-28 14:27:23 -0400

    add index.html to docs
    Co-authored-by: Christopher Genovese <genovese@cmu.edu>

---
+++ scripts/build_index_html.js
@@ -0,0 +1,18 @@
+const { readFileSync, writeFileSync } = require('fs');
+const { join } = require('path');
+const MarkdownIt = require('markdown-it');
+const { version } = require('./../package.json');
+
+const md = new MarkdownIt({
+    html: true,
+});
+
+
+const readme = readFileSync( join( __dirname, '..', 'README.md' ), 'utf-8' );
+
+const indexTemplate = readFileSync( join( __dirname, 'templates', 'index.html' ), 'utf-8' );
+const html = md.render( readme );
+
+const index = indexTemplate.replace( '{{content}}', html );
+
+writeFileSync( join( __dirname, '..', 'docs', 'index.html' ), index );
